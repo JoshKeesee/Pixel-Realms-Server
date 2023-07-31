5f59fbe406d982aca06905852ae3c38a3a392b2f
JoshKeesee <105324950+JoshKeesee@users.noreply.github.com>
2023-07-31 00:48:34 +0000

    .

--- commands/online.js
+++ commands/online.js
@@ -3,9 +3,13 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
 	data: new SlashCommandBuilder()
 	.setName("online")
-	.setDescription("Check how many players are online"),
+	.setDescription("Check how many players are online")
+	.addBooleanOption(o => o.setName("list_names")
+		.setDescription("List the online player's names")
+		.setRequired(false)
+	),
 	async execute(c, io, players) {
 		const n = players.size;
-		await c.reply({ content: `There ${n == 1 ? "is" : "are"} currently ${n} ${n == 1 ? "player" : "players"} online!`, ephemeral: true });
+		await c.reply({ content: `There ${n == 1 ? "is" : "are"} currently ${n} ${n == 1 ? "player" : "players"} online!${c.options.getBoolean("list_names") ? " (" + [...players.values()].map(p => p.name).join(", ") + ")" : ""}`, ephemeral: true });
 	},
 };
