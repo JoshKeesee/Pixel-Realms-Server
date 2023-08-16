d54aca8757fdb3133565974be6b750627ecb519b
JoshKeesee <105324950+JoshKeesee@users.noreply.github.com>
2023-08-16 02:57:29 +0000

    .

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const Login = require("@jkeesee/login");
 const db = require("@jkeesee/json-db");
 const createId = require("./assets/createId.js");
 const newMap = require("./assets/newMap.js");
+const rooms = db.get("rooms") || {};
+Object.keys(rooms).forEach(k => rooms[k].players = {});
+
+setInterval(() => db.set({ rooms }), 10000);
 
 app.use(express.urlencoded({
 	extended: true,
@@ -27,24 +31,23 @@ io.on("connection", socket => {
 
 	socket.on("login", data => {
 		if (!data) return;
-		user = data;
+		user = { ...data, room: user.room };
 		const p = defaultPlayer(socket.id);
 		p.name = data.name;
 		p.profile = data.profile;
 		p.id = socket.id;
 		p.user = user;
-		user.room = p.room;
+		rooms[user.room].players[socket.id] = p;
+		socket.broadcast.to(user.room).emit("update player", rooms[user.room].players[socket.id]);
 		socket.emit("user", p);
-		socket.broadcast.to(user.room).emit("update player", p);
 	});
 
 	socket.on("logout", () => {
-		const rooms = db.get("rooms") || {};
-		if (!user.id) return;
-		user = {};
+		if (typeof user.id != "number" || !user.room) return;
 		rooms[user.room].players[socket.id] = defaultPlayer(socket.id);
-		socket.emit("user", defaultPlayer(socket.id));
-		socket.broadcast.emit("update player", defaultPlayer(socket.id));
+		socket.broadcast.to(user.room).emit("update player", rooms[user.room].players[socket.id]);
+		socket.emit("user", rooms[user.room].players[socket.id]);
+		user = { room: user.room };
 	});
 
 	socket.on("give item", ([id, item, amount]) => socket.to(id).emit("give item", [item, amount]));
@@ -60,27 +63,21 @@ io.on("connection", socket => {
 		socket.to(id).emit("kick");
 	});
 	socket.on("update daylight", time => {
-		const rooms = db.get("rooms") || {};
 		if (!user.room) return;
 		rooms[user.room].daylight = time;
-		db.set({ rooms });
 		socket.broadcast.to(user.room).emit("update daylight", rooms[user.room].daylight);
 	});
 
 	socket.on("update map", d => {
-		const rooms = db.get("rooms") || {};
 		if (!d || !user.room) return;
 		rooms[user.room].map[d[1]] = d[0];
-		db.set({ rooms });
 		socket.broadcast.to(user.room).emit("update map", d);
 	});
 
 	socket.on("update break", d => {
-		const rooms = db.get("rooms") || {};
 		if (!d || !user.room) return;
 		if (d[0] < 0) delete rooms[user.room].map[d[1]].break[d[2]];
 		else rooms[user.room].map[d[1]].break[d[2]] = d[0];
-		db.set({ rooms });
 		socket.broadcast.to(user.room).emit("update break", d);
 	});
 
@@ -91,44 +88,33 @@ io.on("connection", socket => {
 	});
 
 	socket.on("update entity", data => {
-		const rooms = db.get("rooms") || {};
 		if (!user.room || !data) return;
 		const p = rooms[user.room].players[socket.id];
 		rooms[user.room].map[p.scene].entities[data[1]] = data[0];
-		db.set({ rooms });
-		socket.broadcast.emit("update entity", [...data, p.scene]);
+		socket.broadcast.to(user.room).emit("update entity", [...data, p.scene]);
 	});
 
 	socket.on("remove entity", data => {
-		const rooms = db.get("rooms") || {};
 		if (!user.room || !data) return;
 		const p = rooms[user.room].players[socket.id];
 		rooms[user.room].map[p.scene].entities.splice(data, 1);
-		db.set({ rooms });
-		socket.broadcast.emit("remove entity", [...data, p.scene]);
+		socket.broadcast.to(user.room).emit("remove entity", [...data, p.scene]);
 	});
 
 	socket.on("update player", changes => {
-		const rooms = db.get("rooms") || {};
 		if (!changes || !user.room) return;
 		const p = rooms[user.room].players[socket.id];
 		Object.keys(changes).forEach(k => p[k] = changes[k]);
-		db.set({ rooms });
-		socket.broadcast.emit("update player", { ...changes, id: socket.id });
+		socket.broadcast.to(user.room).emit("update player", { ...changes, id: socket.id });
 	});
 
 	socket.on("disconnect", () => {
-		const rooms = db.get("rooms") || {};
 		if (!user.room) return;
-		const p = rooms[user.room].players[socket.id];
-		if (user) playerRef.child(user.id).set(p);
 		delete rooms[user.room].players[socket.id];
-		db.set({ rooms });
 		io.to(user.room).emit("remove player", socket.id);
 	});
 
 	socket.on("chat message", m => {
-		const rooms = db.get("rooms") || {};
 		if (!m || !user.room) return;
 		const p = rooms[user.room].players[socket.id];
 		io.to(user.room).emit("chat message", {
@@ -140,20 +126,18 @@ io.on("connection", socket => {
 	socket.on("ping", cb => cb());
 
 	socket.on("join room", id => {
-		const rooms = db.get("rooms") || {};
 		if (!rooms[id] || user.room == id) return;
 		if (user.room) socket.leave(user.room);
 		user.room = id;
 		p = defaultPlayer(socket.id);
-		if (user.id) {
+		if (typeof user.id == "number") {
 			p.name = user.name;
 			p.profile = user.profile;
 			p.user = user;
 		}
 		rooms[user.room].players[socket.id] = p;
-		db.set({ rooms });
 		socket.join(user.room);
-		
+
 		socket.emit("update daylight", rooms[user.room].daylight);
 		socket.emit("init map", rooms[user.room].map);
 		socket.emit("init players", rooms[user.room].players);
@@ -162,13 +146,13 @@ io.on("connection", socket => {
 
 	socket.on("leave room", () => {
 		if (!user.room) return;
+		delete rooms[user.room].players[socket.id];
 		socket.leave(user.room);
 		user.room = null;
 	});
 
 	socket.on("create room", (name, public = true) => {
-		if (!user.id) return;
-		const rooms = db.get("rooms") || {};
+		if (typeof user.id != "number") return;
 		const p = defaultPlayer(socket.id);
 		p.user = user;
 		p.name = user.name;
@@ -182,41 +166,42 @@ io.on("connection", socket => {
 			players: { [socket.id]: p },
 			daylight: 0,
 		};
-		db.set({ rooms });
-		socket.leave(user.room);
+		if (user.room) socket.leave(user.room);
 		user.room = roomId;
 		socket.join(user.room);
 		socket.emit("user", p);
-		socket.broadcast.to(user.room).emit("update player", p);
 	});
 
-	socket.on("get rooms", cb => cb(db.get("rooms") || {}));
+	socket.on("get rooms", cb => cb(Object.values(rooms).map(r => r.name)));
 });
 
 const gameLoop = () => {
 	setTimeout(gameLoop, 24 * 60000 / 24);
 	let sleeping = 0;
-	// players.forEach(p => (p.id != "offline" && p.bed) ? sleeping++ : "");
-	// daylight++;
-	// if (daylight >= 24 || (sleeping >= Math.ceil(players.size / 2) && sleeping > 0)) daylight = 0;
-	// map.forEach(m => {
-	// 	if (m.entities.length == 0) return;
-	// 	m.entities.forEach(e => {
-	// 		let moving = false;
-	// 		players.forEach(p => (p.minecart == m.entities.indexOf(e)) ? moving = true : "");
-	// 		if (!moving && e.id != 2) {
-	// 			e.x = e.dx;
-	// 			e.y = e.dy;
-	// 			e.moving = false;
-	// 			io.emit("update entity", [e, m.entities.indexOf(e), map.indexOf(m)]);
-	// 		}
-	// 		if (e.type == "arrow" && !players.get(e.from) || (!e.x || !e.y)) {
-	// 			io.emit("remove entity", [m.entities.indexOf(e), map.indexOf(m)]);
-	// 			m.entities.splice(m.entities.indexOf(e), 1);
-	// 		}
-	// 	});
-	// });
-	// io.emit("update daylight", daylight);
+	Object.keys(rooms).forEach(k => {
+		const r = rooms[k];
+		Object.values(r.players).forEach(p => (p.id != "offline" && p.bed) ? sleeping++ : "");
+		r.daylight++;
+		if (r.daylight >= 24 || (sleeping >= Math.ceil(Object.keys(r.players).length / 2) && sleeping > 0)) daylight = 0;
+		r.map.forEach(m => {
+			if (m.entities.length == 0) return;
+			m.entities.forEach(e => {
+				let moving = false;
+				Object.values(r.players).forEach(p => (p.minecart == m.entities.indexOf(e)) ? moving = true : "");
+				if (!moving && e.id != 2) {
+					e.x = e.dx;
+					e.y = e.dy;
+					e.moving = false;
+					io.to(k).emit("update entity", [e, m.entities.indexOf(e), r.map.indexOf(m)]);
+				}
+				if (e.type == "arrow" && !r.players[e.from] || (!e.x || !e.y)) {
+					io.to(k).emit("remove entity", [m.entities.indexOf(e), r.map.indexOf(m)]);
+					m.entities.splice(m.entities.indexOf(e), 1);
+				}
+			});
+		});
+		io.to(k).emit("update daylight", r.daylight);
+	});
 };
 
 gameLoop();
